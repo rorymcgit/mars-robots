3dd19431dcc9ba4c4f1293302c25860229ea1eb5
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-06-23 21:53:43 +0100

    triggering createGrid, not passing values through

--- js/controllers/GridController.js
+++ js/controllers/GridController.js
@@ -1,4 +1,33 @@
-app.controller("GridController", ["$scope", function($scope) {
-  $scope.grid = createGrid();
-  $scope.square = "----";
-}]);
+// app.controller("GridController", ["$scope", function($scope) {
+//   // $scope.grid = createGrid();
+//   // $scope.square = "----";
+// }]);
+
+app.controller("formCtrl", function($scope) {
+    $scope.inputValue = null;
+    $scope.createGrid = function(coordinates) {
+      console.log("Coordinates are:", coordinates);
+      var xCoord = parseInt(coordinates.split("")[0]);
+      var yCoord = parseInt(coordinates.split("")[1]);
+
+      var grid = {};
+      grid.rows = [];
+
+      for(var i = 0; i < xCoord; i++) {
+        var row = {};
+        row.cells = [];
+
+        // console.log(row);
+
+        for(var j = 0; j < yCoord; j++) {
+          var cell = {};
+          row.cells.push(cell);
+        }
+
+        grid.rows.push(row);
+      }
+      console.log(grid);
+
+      return grid;
+    };
+});
--- js/createGrid.js
+++ js/createGrid.js
@@ -1,25 +1,25 @@
-function createGrid(coordinates = "55") {
-  console.log("Coordinates are:", coordinates);
-  var xCoord = coordinates.split("")[0];
-  var yCoord = coordinates.split("")[1];
-
-  var grid = {};
-  grid.rows = [];
-
-  for(var i = 0; i < xCoord; i++) {
-    var row = {};
-    row.cells = [];
-
-    // console.log(row);
-
-    for(var j = 0; j < yCoord; j++) {
-      var cell = {};
-      row.cells.push(cell);
-    }
-
-    grid.rows.push(row);
-  }
-  console.log(grid);
-
-  return grid;
-}
+// function createGrid(coordinates = "55") {
+//   console.log("Coordinates are:", coordinates);
+//   var xCoord = coordinates.split("")[0];
+//   var yCoord = coordinates.split("")[1];
+//
+//   var grid = {};
+//   grid.rows = [];
+//
+//   for(var i = 0; i < xCoord; i++) {
+//     var row = {};
+//     row.cells = [];
+//
+//     // console.log(row);
+//
+//     for(var j = 0; j < yCoord; j++) {
+//       var cell = {};
+//       row.cells.push(cell);
+//     }
+//
+//     grid.rows.push(row);
+//   }
+//   console.log(grid);
+//
+//   return grid;
+// }
