a4c4c7f2692acf3eb4c44e467b098f314d94e22d
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-06-25 14:28:58 +0100

    remove createGrid.js

--- spec/robotInstructions_spec.js
+++ spec/robotInstructions_spec.js
@@ -1,3 +1,5 @@
+"use strict";
+
 describe("Robot instructions", function() {
   var gridController;
 
--- spec/robotPosition_spec.js
+++ spec/robotPosition_spec.js
@@ -1,3 +1,5 @@
+"use strict";
+
 describe("Robot position", function() {
   var gridController;
 
